1ea529efde77dab2dd5e7f8bbf3ae4a8f958f1ee
Eugene Khom <homyakny@gmail.com>
2017-11-21 00:53:14 -0500

    Added notes and extra javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,69 +3,91 @@
 var words = ["avatar", "titanic", "starwars", "avengers", "transformers", "skyfall", "terminator", "armageddon", "superbad", "it", "matrix"];
 var letters = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
-//.....vars for game stats and other vars
+//..... Global vars for game stats
 
+var randomWord = "";
 var triesLeft = 12;
-var wins = 0;
+var correctAnswers = 0;
 var losses = 0;
-var gameStarted = false;
-var gameStatus = document.getElementById("gameStatus")
-var randomWord = words[Math.floor(Math.random() * words.length)];
 var underScore = [];
 var wrongletter = [];
+var gameStarted = false;
+var gameStatus = document.getElementById("gameStatus")
 
-//....divs fill up
+//....When page loads game stats update
 
 document.getElementById("triesLeft").innerHTML = triesLeft;
-document.getElementById("wins").innerHTML = wins;
+document.getElementById("wins").innerHTML = correctAnswers;
 document.getElementById("losses").innerHTML = losses;
-gameStatus.innerHTML = "<p>Press <b>ENTER</b> to start the game!</p>"
+gameStatus.innerHTML = "<p>Press any button to start the game!</p>"
 
-//....event listener to start the game and generate a random word
+//....event listener to start the game
+
 
 addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.keyCode === 13 && gameStarted === false) {
-        generateRandomWord();
+if (gameStarted === false) {
         gameStarted = true;
-        console.log("Has Game Started? " + gameStarted);
         gameStatus.innerHTML = "<p>You successfully started the game. Good luck!</p>"
-        startTheGame()
-    }
+        startTheGame();
+}
 });
 
-//....random word generator
 
-function generateRandomWord() {
-    randomWord
-    console.log(randomWord)
-}
-
-//.... create underscores and insert them in #guessingWord div
+//.... when key pressed game has started and random word generated
 
 function startTheGame() {
-    if (gameStarted === true) {
-        for (i = 0; i < randomWord.length; i++) {
-            underScore[i] = "_";
-            document.getElementById("guessingWord").innerHTML = underScore.join(" ");
-        }
+
+    var wrongletter = [];
+    var underScore = [];
+    var randomWord = words[Math.floor(Math.random() * words.length)];
+    console.log(randomWord);
+
+    //......for loop to replace number of randomWord letters with underscores      
+
+    for (i = 0; i < randomWord.length; i++) {
+        underScore[i] = "_";
+        document.getElementById("guessingWord").innerHTML = underScore.join(" ");
+
+        //.......event listener to find which letter was pressed by user
+
         addEventListener("keyup", function (event) {
-            triesLeft = "";
             var keyPressed = [event.keyCode];
             var letterPressed = String.fromCharCode(keyPressed).toLowerCase();
             console.log(letterPressed)
+            document.getElementById("triesLeft").innerHTML = triesLeft;
+
+            //.......if right letter guessed, replace underscore with that letter
+
             if (letters.indexOf(letterPressed) >= 0 && randomWord.indexOf(letterPressed) >= 0) {
-                underScore[randomWord.indexOf(letterPressed)] = letterPressed;
-                document.getElementById("guessingWord").innerHTML = underScore.join(" ");
-            }
-            if (letters.indexOf(letterPressed) >= 0 && randomWord.indexOf(letterPressed) <= 0) {
-                if (wrongletter.indexOf(letterPressed) === -1) {
-                    wrongletter.push(letterPressed);
-                    document.getElementById("guessedLetters").innerHTML = wrongletter.join(" ");
-                    triesLeft -=1;
+                for (j = 0; j < underScore.length; j++) {
+                    if (randomWord[j] === letterPressed) {
+                        underScore[j] = letterPressed;
+                    }
+                    document.getElementById("guessingWord").innerHTML = underScore.join(" ");
+                    if (underScore.join("") === randomWord) {
+                        gameStarted = false;
+                        gameStatus.innerHTML = "You win! The word was <b>" + randomWord + "</b>. Press any button to restart."
+                        correctAnswers++;
+                        document.getElementById("wins").innerHTML = correctAnswers;
+                    }
                 }
             }
 
+            //......... if guessed letter is wrong, push it to a wrong array and show it in wrong letter div    
+            else if (letters.indexOf(letterPressed) >= 0 && randomWord.indexOf(letterPressed) <= 0) {
+                if (wrongletter.indexOf(letterPressed) === -1) {
+                    wrongletter.push(letterPressed);
+                    document.getElementById("guessedLetters").innerHTML = wrongletter.join(" ");
+                    triesLeft--;
+                    document.getElementById("triesLeft").innerHTML = triesLeft;
+                    if (triesLeft === 0) {
+                        gameStarted = false;
+                        gameStatus.innerHTML = "You lost! The word was <b>" + randomWord + "</b>. Press any button to restart."
+                        losses++;
+                        document.getElementById("losses").innerHTML = losses;
+                    }
+                }
+            }
         })
     }
 }
